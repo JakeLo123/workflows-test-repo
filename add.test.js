d104334b741aff75e6e0a8ddbbdc3fbdbd3b8440
Jake Loew <jake@madebychalk.com>
2024-03-19 11:05:40 -0600

    update test to pass

--- add.test.js
+++ add.test.js
@@ -2,5 +2,5 @@ import { expect, test } from "vitest";
 import { addNumbers } from "./index";
 
 test("adds 1 + 2 to equal 3", () => {
-  expect(addNumbers(1, 2)).toBe(4);
+  expect(addNumbers(1, 2)).toBe(3);
 });
